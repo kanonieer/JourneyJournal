5d0623bf9cdc56b4c8e2e81a3ce1d429a1c637db
Pawel <kanonieer@o2.pl>
2017-04-20 21:31:30 +0200

    removed unused import line causing problems

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -8,7 +8,6 @@ import { FacebookComponent }    from './facebook/facebook.component';
 import { SignUpLoginComponent } from './sign-up-login/sign-up-login.component';
 
 import { AuthService }          from './auth.service';
-import { AUTH_ROUTES }          from './auth.routing';
 
 
 @NgModule({
---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { apiAdress } from './../shared/globalVariables';
+
+@Injectable()
+export class AuthService {
+
+  constructor(private _http: Http) { }
+
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers });
+
+  facebookAuth():Observable<any>{
+    return this._http.get(apiAdress+'/auth/facebook',this.options)
+      .map((response: Response) => response.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    console.error('Auth Service Error:', error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+}
