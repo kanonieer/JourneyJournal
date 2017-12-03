c5e6b555736e69f7f101b8bd4345768d62b37a08
MrVaath <pat.wylegala@gmail.com>
2017-12-03 12:19:23 +0100

    Delete journey - final (?) fix, add set focus to searchbar

--- mobile/src/pages/journeys/journeys.ts
+++ mobile/src/pages/journeys/journeys.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController, AlertController, ToastController, MenuController, ModalController, Events, ItemSliding } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController, ToastController, MenuController, ModalController, Events, ItemSliding, Searchbar } from 'ionic-angular';
 
 // Pages
 import { AddJourneyPage } from '../addJourney/addJourney';
 import { DetailsJourneyPage } from '../detailsJourney/detailsJourney';
 import { EditJourneyPage } from '../editJourney/editJourney';
+
+// Plugins
+import { Keyboard } from '@ionic-native/keyboard';
 
 // Providers
 import { JourneyService } from '../../providers/journey-service';
@@ -19,6 +22,8 @@ import { Journey } from './../../models/Journey';
 })
 
 export class JourneysPage {
+
+  @ViewChild('search') searchInput: Searchbar;
 
   ionViewDidLoad() {
     this.menuCtrl.enable(true);
@@ -38,7 +43,7 @@ export class JourneysPage {
   };
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public toastCtrl: ToastController, public menuCtrl: MenuController, 
-    public modalCtrl: ModalController, public events: Events, private journeySvc: JourneyService) {
+    public modalCtrl: ModalController, public events: Events, public keyboard: Keyboard, private journeySvc: JourneyService) {
 
     events.subscribe('journey:get', () => {
       this.getJourneys();
@@ -87,11 +92,11 @@ export class JourneysPage {
   }
 
   // Confirm
-  public deleteConfirm(id: String, item: ItemSliding) {
+  public deleteConfirm(id: String, title: String, item: ItemSliding) {
     item.close();
     const alert = this.alertCtrl.create({
       title: 'Confirm delete',
-      message: 'Do you want to delete this journey?',
+      message: 'Do you want to delete ' + title + '?',
       buttons: [
         {
           text: 'Cancel',
@@ -111,20 +116,18 @@ export class JourneysPage {
   // Delete
   public deleteJourney(id: String) {
     // 'for' loop through the list, and delete selected item
-    for(let i = 0; i < this.journeys.length; i++) {
-      for(let j = 0; j < this.loadedJourneys.length; j++) {
-        if((this.journeys[i]._id === id) && (this.loadedJourneys[j]._id === id)) {
-          this.journeySvc.deleteJourney(id).subscribe(
-            (result) => {
-              this.journeys.splice(i, 1);
-              this.loadedJourneys.splice(j, 1);
-              this.presentToastSuccess(result);
-            },
-            (error) => {
-              this.presentToastError(error);
-            }
-          );
-        }
+    for(let i = 0; i < this.loadedJourneys.length; i++) {
+      if(this.loadedJourneys[i]._id === id) {
+        this.journeySvc.deleteJourney(id).subscribe(
+          (result) => {
+            this.loadedJourneys.splice(i, 1);
+            this.toggleSearchbarOff();
+            this.presentToastSuccess(result);
+          },
+          (error) => {
+            this.presentToastError(error);
+          }
+        );
       }
     }
   }
@@ -156,6 +159,13 @@ export class JourneysPage {
   // On
   toggleSearchbarOn() {
     this.showSearchbar = true;
+    setTimeout(() => {
+      this.searchInput.setFocus();
+      this.keyboard.show();
+    }, 100);
+    this.keyboard.onKeyboardShow().subscribe((data) => {
+      this.searchInput.setFocus();
+    });
   }
 
   // Off
@@ -173,16 +183,11 @@ export class JourneysPage {
   // Search
   getItems(searchbar) {
     this.initializeItems();
-    let typedValue = searchbar.srcElement.value;
+    let typedValue = searchbar.target.value;
     // trim => remove whitespace from both sides of a String
     if (typedValue && typedValue.trim() !== '') {
       this.journeys = this.journeys.filter((journey) => {
-        if(journey.title && typedValue) {
-          if(journey.title.toLowerCase().indexOf(typedValue.toLowerCase()) > -1) {
-            return true;
-          }
-          return false;
-        }
+        return (journey.title.toLowerCase().indexOf(typedValue.toLowerCase()) > -1);
       });
     }
   }
