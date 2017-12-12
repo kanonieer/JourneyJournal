e24ef4f5cdc8b3db53b0b8fb77a51b4ea689bb49
MrVaath <pat.wylegala@gmail.com>
2017-12-12 02:09:47 +0100

    fix import serverAdress

--- mobile/src/providers/account-service.ts
+++ mobile/src/providers/account-service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 // Shared
-import { serverAdress } from './../shared/globalVariables';
+import { serverAdress } from './../shared/GlobalVariables';
 import { successHandle, errorHandle, errorHandleBody } from './../shared/Handler';
 
 // Providers
